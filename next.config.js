d93815ea99650bafd5cd62bba898ee44b7e63b90
野村征遵 <nomurasusumushitagau@users-MacBook-Air.local>
2025-09-29 18:06:09 +0900

    Fix vercel.json schema validation error

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  // 大きなファイルのアップロード対応
-  api: {
-    bodyParser: {
-      sizeLimit: '100mb',
-    },
-  },
 }
 
 module.exports = nextConfig
